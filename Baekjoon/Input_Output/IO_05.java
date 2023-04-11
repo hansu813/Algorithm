2e846585fa850c8f3b9f2f92aa24e92a8cbdb398
hansu813 <108872334+hansu813@users.noreply.github.com>
2023-04-11 20:29:10 +0900

    Add files via upload

---
+++ Baekjoon/Input_Output/IO_05.java
@@ -0,0 +1,14 @@
+package java_basic;
+/**
+ * 백준 10171번
+ * 고양이를 출력하시오.
+ * @author 김한수
+ */
+public class IO_05 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
